95f6073aa7e641a00026d9b98c80b7bc9b3f2218
Hiroshi-N-S <hiroshi.n.september11@gmail.com>
2026-07-20 13:17:17 +0900

    feat: integrate Keycloak OIDC client secrets with Vault KV2
    - Add Vault provider configuration and token loading logic to `provider.tf`
    - Add `vault_kv_v2_mount_path` to realm variables to specify the Vault mount
    - Update OIDC client resources to fetch `client_secret` from Vault instead of variables
    - Enforce `access_type = "CONFIDENTIAL"` for all managed clients
    - Introduce `vault_provider_config` to input variables and sample tfvars

--- iac/keycloak/openid_clients.tf
+++ iac/keycloak/openid_clients.tf
@@ -2,6 +2,41 @@
 # OpenID Connect Clients
 #
 
+# Get client secret from Vault (ephemeral)
+ephemeral "vault_kv_secret_v2" "keycloak_client_secrets" {
+  for_each = {
+    for i in flatten([
+      for realm_key, realm_value in var.realms : [
+        for client_key, client_value in realm_value.clients : {
+          key   = "${realm_key}-${client_key}"
+          name  = "${client_key}-clientsecret"
+          mount = "${realm_value.vault_kv_v2_mount_path}"
+        }
+      ]
+    ]) : i.key => i
+  }
+
+  mount = each.value.mount
+  name  = each.value.name
+}
+
+# DEPRECATED: Get client secret from Vault for version management
+data "vault_kv_secret_v2" "keycloak_client_secrets" {
+  for_each = {
+    for i in flatten([
+      for realm_key, realm_value in var.realms : [
+        for client_key, client_value in realm_value.clients : {
+          key  = "${realm_key}-${client_key}"
+          name = "${client_key}-clientsecret"
+        }
+      ]
+    ]) : i.key => i
+  }
+
+  mount = "k8s-secrets"
+  name  = each.value.name
+}
+
 resource "keycloak_openid_client" "oidc_clients" {
   for_each = {
     for i in flatten([
@@ -21,7 +56,9 @@ resource "keycloak_openid_client" "oidc_clients" {
   enabled                         = each.value.client_data.enabled
   name                            = each.value.client_data.name
   description                     = each.value.client_data.description
-  access_type                     = each.value.client_data.access_type
+  access_type                     = "CONFIDENTIAL"
+  client_secret_wo                = ephemeral.vault_kv_secret_v2.keycloak_client_secrets[each.key].data["secret"]
+  client_secret_wo_version        = data.vault_kv_secret_v2.keycloak_client_secrets[each.key].version
   standard_flow_enabled           = each.value.client_data.standard_flow_enabled
   implicit_flow_enabled           = each.value.client_data.implicit_flow_enabled
   direct_access_grants_enabled    = each.value.client_data.direct_access_grants_enabled
--- iac/keycloak/provider.tf
+++ iac/keycloak/provider.tf
@@ -14,8 +14,34 @@ terraform {
 }
 
 provider "keycloak" {
-  client_id                 = var.provider_config.client_id
-  client_secret             = var.provider_config.client_secret
-  url                       = var.provider_config.url
-  tls_insecure_skip_verify  = var.provider_config.tls_insecure
+  client_id                = var.provider_config.client_id
+  client_secret            = var.provider_config.client_secret
+  url                      = var.provider_config.url
+  tls_insecure_skip_verify = var.provider_config.tls_insecure
+}
+
+#
+# Vault Provider Configuration
+#
+
+locals {
+  # Read Vault token from a file.
+  vault_config_file = fileexists("~/.vault-initial.config") ? file("~/.vault-initial.config") : ""
+
+  # Read a variable from a variable or `.vault-initial.config` and set it as the token for authentication. This allows for flexible configuration of the Vault token, enabling users to either store it in a file or set it as an environment variable.
+  vault_token = var.vault_provider_config.token != "" ? var.vault_provider_config.token : (
+    local.vault_config_file != "" ? trimspace(
+      regex("(?m)^Initial Root Token:\\s*([^\\n]+)", local.vault_config_file)[0]
+    ) : ""
+  )
+}
+
+#
+# Vault
+#
+
+provider "vault" {
+  address         = var.vault_provider_config.url
+  skip_tls_verify = var.vault_provider_config.skip_tls_verify
+  token           = local.vault_token
 }
