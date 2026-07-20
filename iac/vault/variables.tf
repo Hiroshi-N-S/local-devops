5de72739cdf38a54e3aca30f85662a2734f5a568
Hiroshi-N-S <hiroshi.n.september11@gmail.com>
2026-07-20 11:44:51 +0900

    feat: automate OIDC client secret generation using random passwords
    - Add Terraform `random_password` to securely generate secrets
    - Modify Vault secret processing to resolve `random_password:` prefixes
    - Update variable definitions and sample tfvars to configure and apply the new random passwords

--- iac/vault/variables.tf
+++ iac/vault/variables.tf
@@ -3,26 +3,31 @@
 #
 
 variable "kubernetes_auth" {
-  description                          = "Kubernetes Auth Method configuration"
-  type                                 = object({
-    config                             = object({
-      host                             = optional(string, "https://kubernetes.default.svc.cluster.local")
-      ca_cert                          = optional(string, "")
-      token_reviewer_jwt               = optional(string, "")
-      issuer                           = optional(string, "api")
-      disable_iss_validation           = optional(bool, true)
+  description = "Kubernetes Auth Method configuration"
+  type = object({
+    config = object({
+      host                   = optional(string, "https://kubernetes.default.svc.cluster.local")
+      ca_cert                = optional(string, "")
+      token_reviewer_jwt     = optional(string, "")
+      issuer                 = optional(string, "api")
+      disable_iss_validation = optional(bool, true)
     })
-    role                               = object({
+    role = object({
       name                             = string
       bound_service_account_names      = list(string)
       bound_service_account_namespaces = list(string)
       token_ttl                        = optional(number, 3600)
     })
-    secret_engines_and_policies        = map(object({
-      description                      = string
-      path                             = optional(string, "data/*")
-      capabilities                     = optional(list(string), ["read", "list"])
-      data                             = map(map(string))
+    secret_engines_and_policies = map(object({
+      description  = string
+      path         = optional(string, "data/*")
+      capabilities = optional(list(string), ["read", "list"])
+      random_passwords = map(object({
+        length           = optional(number, 32)
+        special          = optional(bool, true)
+        override_special = optional(string, "")
+      }))
+      data = map(map(string))
     }))
   })
 }
@@ -32,8 +37,8 @@ variable "kubernetes_auth" {
 #
 
 variable "provider_config" {
-  description       = "Vault provider configuration"
-  type              = object({
+  description = "Vault provider configuration"
+  type = object({
     url             = string
     token           = optional(string, "")
     skip_tls_verify = optional(bool, false)
